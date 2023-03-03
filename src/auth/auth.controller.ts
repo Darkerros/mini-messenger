23c6c3d5211fd7059b419d20b084a0551654ae91
darker <darkerros@yandex.ru>
2023-03-03 18:40:00 +0300

    feat: create login and register functionality on auth module controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from "./auth.service";
+import {CreateUserDto} from "../users/dto/create-user.dto";
+import {LoginUserDto} from "../users/dto/login-user.dto";
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @Post("/register")
+    async register(@Body() dto: CreateUserDto) {
+        return await this.authService.register(dto)
+    }
+
+    @Post("/login")
+    async login(@Body() dto: LoginUserDto) {
+        return await this.authService.login(dto)
+    }
+
+}
