fd07d060133e1c03bdc97b216c7ce2d792731261
darker <darkerros@yandex.ru>
2023-03-03 21:31:26 +0300

    feat: create auth guard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import {CanActivate, ExecutionContext, Injectable, UnauthorizedException} from "@nestjs/common";
+import {Observable} from "rxjs";
+import {AuthService} from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private authService: AuthService) {}
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        try {
+            const req = context.switchToHttp().getRequest()
+            const splitedAuthHeader = req.headers.authorization.split(" ")
+            //const tokenType = splitedAuthHeader[0]
+            const token = splitedAuthHeader[1]
+
+            req.user = this.authService.validateToken(token)
+            return true
+        }
+        catch (e) {
+            throw new UnauthorizedException("Пользователь не авторизован")
+        }
+    }
+
+}
