dfad52107e15faba08c5a411029d845b228db300
darker <darkerros@yandex.ru>
2023-03-03 18:11:32 +0300

    feat: add register, login functionality to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
 import {JwtService} from "@nestjs/jwt";
+import {CreateUserDto} from "../users/dto/create-user.dto";
+import {LoginUserDto} from "../users/dto/login-user.dto";
+import {UsersService} from "../users/users.service";
+import {UserModel} from "../models/user.model";
+import * as bcrypt from "bcrypt"
 
 @Injectable()
 export class AuthService {
-    constructor(private jwtService: JwtService) {}
+    constructor(private jwtService: JwtService, private usersService: UsersService) {}
 
-    async register() {
+    async register(dto: CreateUserDto) {
+        const candidate = this.usersService.getUserByEmail(dto.email)
+        if (candidate) throw new HttpException("Пользователь с таким email уже существует", HttpStatus.BAD_REQUEST)
 
+        const hashedPassword = await bcrypt.hash(dto.password, 5)
+        const user = await this.usersService.createUser({...dto, password: hashedPassword})
+        return this.generateToken(user)
     }
 
-    async login() {
-
+    async login(dto: LoginUserDto) {
+        const user = await this.validateUser(dto)
+        return this.generateToken(user)
     }
 
-    async validateToken() {
-        return
+    async validateUser(dto: LoginUserDto) {
+        const user = await this.usersService.getUserByEmail(dto.emaiL)
+        const isPasswordEquals = bcrypt.compare(dto.password, user.password)
+        if (user && isPasswordEquals) return user
+        throw new UnauthorizedException({ message: "Некорректо введен email или пароль"})
     }
 
-    async generateToken() {
-        return {}
+    validateToken(token: string) {
+        return this.jwtService.verify(token)
+    }
+
+    generateToken(user: UserModel) {
+        const payload = {id: user.id, email: user.email}
+        const token = this.jwtService.sign(payload)
+        return { token: token , user: { id: user.id, email: user.email }}
     }
 }
