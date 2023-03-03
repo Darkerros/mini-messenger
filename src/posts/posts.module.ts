f3e9d81bf4699d44ffbab582345ffbbe23eb6007
darker <darkerros@yandex.ru>
2023-03-03 23:05:29 +0300

    feat: add post like post comment service in post module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { PostModel } from '../models/post.model';
+import { PostLikeModel } from '../models/post-like.model';
+import { PostCommentModel } from '../models/post-comment.model';
+import { AuthModule } from '../auth/auth.module';
+import { PostsLikeService } from './posts-like.service';
+import { PostsCommentService } from './posts-comment.service';
 
 @Module({
-  providers: [PostsService],
+  providers: [PostsService, PostsLikeService, PostsCommentService],
   controllers: [PostsController],
+  imports: [
+    SequelizeModule.forFeature([PostModel, PostLikeModel, PostCommentModel]),
+    AuthModule,
+  ],
 })
 export class PostsModule {}
