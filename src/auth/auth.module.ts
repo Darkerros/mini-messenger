d65b5ea58e262f15d6e3335d9e021403794c4765
darker <darkerros@yandex.ru>
2023-03-03 18:39:32 +0300

    feat: fix get secret key from env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import {JwtModule} from "@nestjs/jwt";
-import * as process from "process";
-import {UsersModule} from "../users/users.module";
+import { JwtModule } from "@nestjs/jwt";
+import { UsersModule } from "../users/users.module";
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
   imports: [
       JwtModule.register({
-        secret: process.env.SECTRET_KEY
+        secret: `${process.env.SECTRET_KEY}`,
       }),
       UsersModule,
   ],
